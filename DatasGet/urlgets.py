196b88b6282b634096f761f0d7a029a1d249d354
Top Programmer - Hacker <63028057+tph-ux@users.noreply.github.com>
2021-02-15 10:18:37 +0800

    GetsUrl
    Get url requests, use lxml return xpath string value.

--- DatasGet/urlgets.py
+++ DatasGet/urlgets.py
@@ -1,1 +1,19 @@
+# -*- coding:utf-8 -*-
+# @Time:     2021/2/12 22:54
+# @Author:   Top Programmer - Hacker(Administrator)
+# @Software: PyCharm
+import requests
+from lxml import etree
+import my_fake_useragent as mfu
 
+headers = {
+    "Referer": "http://bitpush.news/covid19/",
+    "User-Agent": mfu.UserAgent().random()
+}
+
+gets = requests.get("http://bitpush.news/covid19/", headers=headers).text
+
+
+def x_paths(ps):
+    html = etree.HTML(gets)
+    return html.xpath(ps)
